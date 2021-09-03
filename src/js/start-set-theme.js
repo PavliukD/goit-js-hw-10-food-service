a0f068d1a18f2614cfe25abcb222384251b2ad48
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2021-09-03 09:39:11 +0300

    Добавлено сохранение темы при перезагрузке

--- src/js/start-set-theme.js
+++ src/js/start-set-theme.js
@@ -1,19 +1,10 @@
 const checkboxInput = document.querySelector('#theme-switch-toggle')
-const body = document.querySelector('body')
 let startTheme = JSON.parse(localStorage.getItem('themeSwitchToggle'))
-console.log(startTheme)
 
 export default function startSetTheme(){
-    if (startTheme = true) {
-        body.classList.remove('light-theme')
-        body.classList.add('dark-theme')
+    if (startTheme){
         checkboxInput.checked = true
-        localStorage.setItem('themeSwitchToggle', 'true')
-    } else {
-        body.classList.remove('dark-theme')
-        body.classList.add('light-theme')
-        checkboxInput.checked = false
-        localStorage.setItem('themeSwitchToggle', 'false')
     }
+
 }
 
